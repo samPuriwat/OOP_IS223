93f32878b93933a6d2b04862ccd731fea8a4d7fd
samPuriwat <sam.puriwat@gmail.com>
2017-11-17 10:31:25 +0700

    Testing upload to Github

---
+++ src/oop_lab/studentis223/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,10 @@
+package oop_lab.studentis223.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm SAM");
+    }// main
+
+
+} // class
