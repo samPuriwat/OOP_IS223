fe32392a4a5b5f1d46f10f84165393e272cf65d9
samPuriwat <sam.puriwat@gmail.com>
2017-11-17 11:18:48 +0700

    Testing edit code

--- src/oop_lab/studentis223/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/studentis223/rmutsv/com/myFirstJAVA.java
@@ -5,8 +5,7 @@ public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Wordl, I'm SAM");
         System.out.println("Information System");
-        System.out.println("Faculty of Management Technololgy");
-        System.out.println("RMUTSV");
+
     }// main
 
 
