056d5e8111eaccbd4b8dce69a24f7d748741c98f
samPuriwat <sam.puriwat@gmail.com>
2017-12-11 15:40:09 +0700

    Test While loop

---
+++ src/ooplab3/TestWhileLoop.java
@@ -0,0 +1,28 @@
+package ooplab3;
+
+public class TestWhileLoop {
+    public static void main(String[] args) {
+        //while
+        int i = 1;
+        while (i<=100){
+            if (i%3==0&&i%5==0)
+            System.out.print(i+" ");
+            i++;
+        }//while
+//        do-while
+        System.out.println();
+        int j = 1;
+        do{
+            if  (j%3==0&&j%5==0)
+                System.out.print(j+" ");
+        }while (++j<=100);
+
+
+
+
+    }//main
+
+
+
+
+}//class
