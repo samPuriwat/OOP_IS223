01b348dfa9f9fdbf140cfe0b186f19db7007bf02
samPuriwat <sam.puriwat@gmail.com>
2018-02-02 10:29:56 +0700

    Abstract Class: Pet

---
+++ src/ooplab8/Dog.java
@@ -0,0 +1,15 @@
+package ooplab8;
+
+// Dog is a Pet
+// Dog is sub-class
+// Pet is super-class
+public class Dog extends Pet {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Box Box !!!");
+    }
+}
---
+++ src/ooplab8/PetApp.java
@@ -0,0 +1,16 @@
+package ooplab8;
+
+public class PetApp {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("kaya", 2);
+        System.out.println(dog.getName());
+        System.out.println(dog.getAge());
+        dog.makeNoise();
+
+
+
+
+
+    }
+}//class
